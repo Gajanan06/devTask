b78d7ca1068c0655237814bce0a3466244f7f9ff
Gajanan Nilajkar <gajanannilajkar06@gmail.com>
2026-05-20 15:20:53 +0530

    Built landing page with routing navigation

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,50 @@
+import { useNavigate } from "react-router-dom";
+
 import MobileLayout from "../components/MobileLayout";
+import Button from "../components/Button";
+
+import "./style2.css";
 
 const Landing = () => {
+  const navigate = useNavigate();
+
   return (
     <MobileLayout>
-      <h1>Landing Page</h1>
+      <div className="landing-container">
+
+        <div className="landing-content">
+
+          <div className="landing-text">
+
+            <h1>Welcome to devtask</h1>
+
+            <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit.
+            </p>
+
+          </div>
+
+          <div className="landing-buttons">
+
+            <Button
+              text="Create Account"
+              backgroundColor="#6C25FF"
+              textColor="#FFFFFF"
+              onClick={() => navigate("/signup")}
+            />
+
+            <Button
+              text="Already Registered? Login"
+              backgroundColor="#CEBAFB"
+              textColor="#1D2226"
+              onClick={() => navigate("/login")}
+            />
+
+          </div>
+
+        </div>
+      </div>
     </MobileLayout>
   );
 };
