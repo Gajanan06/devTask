66b832898e0dc2f3cb9ea420b9c0276344f4d54e
Gajanan Nilajkar <gajanannilajkar06@gmail.com>
2026-05-20 14:23:48 +0530

    Setup project architecture and routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { RouterProvider } from "react-router-dom";
 import App from './App'
+import React from 'react';
+import "./styles/global.css";
+
+import router from './routes/AppRoute'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
